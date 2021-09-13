6397bbee0616c46e168f90da8309389c9a4a4658
unknown <omarsefo7@gmail.com>
2021-09-13 17:42:25 +0300

    update

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['sendf'])){
+   $name= filter_var( $_POST['name'],FILTER_SANITIZE_STRING);
+   $email = filter_var( $_POST['email'] ,FILTER_SANITIZE_EMAIL) ;
+   $message =filter_var( $_POST['message'] ,FILTER_SANITIZE_STRING) ;
+
+   $headers ='from: ' . $email . '\r\n';
+   $subject = "mail from webpage";
+   $to="omarsefo7@gmail.com";
+   $tXt="Name =". $name . "\r\n Email = ". $email . "\r\n Message = ". $message;
+
+   if ($email!=NULL) {
+      mail($to,$subject,$tXt,$headers);
+   }
+}
+     header("Location:omarsefo.github.io/portfolio/");
+?>
