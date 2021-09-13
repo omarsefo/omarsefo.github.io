8da857d9b87decd3c1c5b14b3f8a6ccc3f05dcf6
unknown <omarsefo7@gmail.com>
2021-09-13 17:56:13 +0300

    update

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST['sendf'])){
+if (isset($_POST["submit"])){
    $name= filter_var( $_POST['name'],FILTER_SANITIZE_STRING);
    $email = filter_var( $_POST['email'] ,FILTER_SANITIZE_EMAIL) ;
    $message =filter_var( $_POST['message'] ,FILTER_SANITIZE_STRING) ;
@@ -13,5 +13,5 @@ if (isset($_POST['sendf'])){
       mail($to,$subject,$tXt,$headers);
    }
 }
-     header("Location:omarsefo.github.io/portfolio/");
+     header("Location:omarsefo.github.io/portfolio/index.html");
 ?>
