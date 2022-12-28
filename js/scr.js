60aef136b89edee63241b29b5859eab1c9e5d023
unknown <omarsefo7@gmail.com>
2022-12-28 18:08:37 +0300

    upgrade

--- js/scr.js
+++ js/scr.js
@@ -244,7 +244,7 @@ document.getElementById("card10").addEventListener('click', () => {
 });
 
 // Right Click disable
-// document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
 
 // copy 
