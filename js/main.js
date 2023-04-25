99803eed5df3ad3016b8b6e20cfc29de695bbdbb
unknown <omarsefo7@gmail.com>
2023-04-25 17:45:46 +0300

    upgrade

--- js/main.js
+++ js/main.js
@@ -49,6 +49,17 @@ window.addEventListener("focus", () =>{
   document.title = docTitle;
 })
 
+window.onload = function () {
+Particles.init({
+  selector: ".background",
+  connectParticles: false,
+  maxParticles: 170,
+  color: "#ffd558",
+  speed: 1,
+  sizeVariations: 4,
+});
+}
+
 class ArrowPointer {
   constructor() {
     this.root = document.body;
